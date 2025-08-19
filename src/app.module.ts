edbd954ac3f4e7ff7bb672f14409cb5a48659a68
Enzo Bersano <bersanoenzo@gmail.com>
2025-08-19 01:21:36 -0300

    agrego module pokemon y prisma, cambios en main.ts y app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    PokemonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api');
+
+    app.useGlobalPipes(new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+    }));
+
+    const config = new DocumentBuilder()
+        .setTitle('Pokémon API')
+        .setDescription('RESTful CRUD with pagination & filtering')
+        .setVersion('1.0.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>('PORT') ?? 3000;
+    await app.listen(port);
 }
 bootstrap();
